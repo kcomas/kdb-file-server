65438aa93f63bb243244ad753798c38d5ab15a0d
kcomas <kevin.comas.git@gmail.com>
2017-06-24 16:40:27 -0400

    add space in struct init

--- src/header.c
+++ src/header.c
@@ -6,5 +6,5 @@ const static int kdbfs_status_messages_length = 3;
 const static struct KDBFS_Status_Messages kdbfs_status_messages[] = {
     {200, "OK"},
     {404, "Not Found"},
-    {500,"Internal Server Error"}
+    {500, "Internal Server Error"}
 };
