551db70c49059db204a495b95774425a5ea28bde
kcomas <kevin.comas.git@gmail.com>
2017-06-24 13:29:03 -0400

    add function to join strings by a char

--- src/util.c
+++ src/util.c
@@ -17,3 +17,23 @@ bool kdbfs_join_strings(char** dest, const char* str1, const char* str2) {
 
     return true;
 }
+
+bool kdbfs_join_strings_by_char(char** dest, const char* str1, const char* str2, const char join) {
+
+    const size_t join_len = 1;
+
+    const size_t len1 = strlen(str1);
+    const size_t len2 = strlen(str2);
+
+    *dest = (char*) malloc(len1 + join_len + len2 + 1);
+
+    if (!dest) {
+        return false;
+    }
+
+    memcpy(*dest, str1, len1);
+    (*dest)[len1] = join;
+    memcpy(*dest + len1 + join_len, str2, len2 + 1);
+
+    return true;
+}
--- src/util.h
+++ src/util.h
@@ -7,4 +7,6 @@
 
 bool kdbfs_join_strings(char** dest, const char* str1, const char* str2);
 
+bool kdbfs_join_strings_by_char(char** dest, const char* str1, const char* str2, const char join);
+
 #endif
